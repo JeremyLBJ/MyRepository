01b9557a23579d51eb289357d26fa6c16020d3a2
佳太 <846722168@qq.com>
2019-08-23 19:20:47 +0800

    将springboot创建好项目首次提交

---
+++ learning_shop/src/main/java/com/hnit/LearningShopApplication.java
@@ -0,0 +1,13 @@
+package com.hnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningShopApplication.class, args);
+	}
+
+}
